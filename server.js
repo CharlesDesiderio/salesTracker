b8f365b798d1f0ade44447b6add176dadab1ba3f
Charles Desiderio <charlesdesiderio@MacBook-of-the-Dead.local>
2020-10-30 10:17:42 -0500

    Added DB connection and USER schema

--- server.js
+++ server.js
@@ -1,22 +1,45 @@
-// CONFIGURATION
-require('dotenv').config()
-const PORT = 3000//process.env.PORT
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const methodOverride = require('method-override')
+// DEPENDENCIES
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require('express-session')
+const methodOverride = require("method-override");
 
+// CONFIGURATION
+require("dotenv").config();
+const PORT = process.env.PORT;
+const app = express();
+const db = mongoose.connection
+const mongoConnection = `${process.env.MONGO_URI}/salesTracker`
 
 // MIDDLEWARE
-app.use(express.static('public'))
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
-app.get('/', (req, res) => {
-  res.send('Connection working')
+// DATABASE CONNECTION
+mongoose.connect(mongoConnection, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+}, () => {
+  console.log(`Connected to MONGODB at ${mongoConnection}`)
 })
+
+// BASIC ROUTES
+app.get("/", (req, res) => {
+  res.send("Connection working");
+});
+
+
 
 // LISTENER
 app.listen(PORT, () => {
-  console.log(`Listening for connections on ${PORT}`)
-})
+  console.log(`Listening for connections on ${PORT}`);
+});

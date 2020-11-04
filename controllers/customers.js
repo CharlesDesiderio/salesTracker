608197baa53e6099350b5de88c94c5522b9ca0ed
Charles Desiderio <charlesdesiderio@MacBook-of-the-Dead.local>
2020-11-03 22:55:19 -0600

    Customer view displays order history data

--- controllers/customers.js
+++ controllers/customers.js
@@ -4,6 +4,7 @@ const Customer = require('../models/customers.js')
 // const Product = require('../models/products.js')
 const User = require('../models/users.js')
 const methodOverride = require('method-override')
+const Product = require('../models/products.js')
 const customers = express.Router()
 customers.use(methodOverride('_method'))
 
@@ -29,10 +30,14 @@ customers.post('/new', (req, res) => {
 })
 
 customers.get('/:id', isAuthenticated, (req, res) => {
-  Customer.findById(req.params.id, (err, foundCustomer) => {
-    res.render('customer/viewCustomer.ejs', {
-      customer: foundCustomer
+  Product.find({ ownerUsername: req.session.currentUser }, (err, foundProducts) => {
+    Customer.findById(req.params.id, (err, foundCustomer) => {
+      res.render('customer/viewCustomer.ejs', {
+        customer: foundCustomer,
+        products: foundProducts
+      })
     })
+
   })
 })
 

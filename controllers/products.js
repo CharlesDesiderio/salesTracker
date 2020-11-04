1602de7c9a735209f11b0f29ca795530bf4471c7
Charles Desiderio <charlesdesiderio@MacBook-of-the-Dead.local>
2020-11-03 21:57:44 -0600

    Can create order for customer

--- controllers/products.js
+++ controllers/products.js
@@ -3,6 +3,7 @@ const express = require('express')
 const User = require('../models/users.js')
 const Product = require('../models/products.js')
 const methodOverride = require('method-override')
+const Customer = require('../models/customers.js')
 const products = express.Router()
 products.use(methodOverride('_method'))
 
@@ -61,6 +62,27 @@ products.post('/new', isAuthenticated, (req, res) => {
 products.delete('/:id', isAuthenticated, (req, res) => {
   Product.findByIdAndDelete(req.params.id, (err, deletedProduct) => {
     res.redirect('/users/profile')
+  })
+})
+
+products.get('/newPurchase/:id', isAuthenticated, (req, res) => {
+  Product.find({ ownerUsername: req.session.currentUser }, (err, foundProducts) => {
+    console.log(foundProducts)
+    res.render('product/newPurchase.ejs', {
+      customerId: req.params.id,
+      products: foundProducts
+    })
+  })
+})
+
+products.put('/newPurchase/:id', isAuthenticated, (req, res) => {
+  req.body.datePurchased = Date.now()
+  Product.findById(req.body.productId, (err, foundItem) => {
+    req.body.purchasePrice = foundItem.price
+    Customer.findByIdAndUpdate(req.params.id, { $push: { purchaseHistory: req.body } }, (err, foundCustomer) => {
+      console.log(req.body)
+      res.redirect('/users/profile')
+    })
   })
 })
 
